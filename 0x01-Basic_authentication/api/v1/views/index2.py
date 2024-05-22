95c8542525fc81d1adc18c897eee3e559706e2d2
stanley <stanleyibeneme92@gmail.com>
2024-05-21 18:21:30 -0700

    index

--- 0x01-Basic_authentication/api/v1/views/index2.py
+++ 0x01-Basic_authentication/api/v1/views/index2.py
@@ -5,27 +5,6 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-# unauthorized access
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """_summary_
-
-    Returns:
-        str: _description_
-    """
-    abort(401, description='Unauthorized')
-
-# forbidden
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """_summary_
-
-    Returns:
-        str: _description_
-    """
-    abort(403, description='Forbidden')
-
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -45,3 +24,4 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
