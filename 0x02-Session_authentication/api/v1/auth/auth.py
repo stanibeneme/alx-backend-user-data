598c14c2bfe6ef7df3cfdd193c629681b170d55c
stanley <stanleyibeneme92@gmail.com>
2024-05-23 06:19:42 -0700

    chmm

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,68 +1,92 @@
 #!/usr/bin/env python3
-"""
-Module for authentication
-"""
-
-
-from typing import List, TypeVar
+"""Auth class"""
 from flask import request
+from flask import Flask
+from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
-    """_summary_
-    """
+    """class for handling auth"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """_summary_
+        """handles auth
 
         Args:
-                path (str): _description_
-                excluded_paths (List[str]): _description_
+            path (str):
+            excluded_paths (List[str]): _description_
 
         Returns:
-                bool: _description_
+            bool:
         """
-        if path is None:
+        if path is None or excluded_paths is None or not excluded_paths:
             return True
 
-        if excluded_paths is None or excluded_paths == []:
-            return True
+        # handle * at end of excluded paths
+        if path[-1] == '/':
+            path = path[:-1]
+
+        contains_slash = False
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '/':
+                excluded_path = excluded_path[:-1]
+                contains_slash = True
+
+            if excluded_path.endswith('*'):
+                idx_after_last_slash = excluded_path.rfind('/') + 1
+                excluded = excluded_path[idx_after_last_slash:-1]
+
+                idx_after_last_slash = path.rfind('/') + 1
+                tmp_path = path[idx_after_last_slash:]
+
+                if excluded in tmp_path:
+                    return False
+
+            if contains_slash:
+                contains_slash = False
+
+        path += '/'
 
         if path in excluded_paths:
             return False
 
-        for excluded_path in excluded_paths:
-            if excluded_path.startswith(path):
-                return False
-            elif path.startswith(excluded_path):
-                return False
-            elif excluded_path[-1] == "*":
-                if path.startswith(excluded_path[:-1]):
-                    return False
-
         return True
 
     def authorization_header(self, request=None) -> str:
-        """_summary_
+        """handle authorization header
 
         Args:
-                request (_type_, optional): _description_. Defaults to None.
+            request (_type_, optional): _description_. Defaults to None.
 
         Returns:
-                str: _description_
+            str: _description_
         """
         if request is None:
             return None
-        # get header from the request
-        header = request.headers.get('Authorization')
 
-        if header is None:
-            return None
-
-        return header
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """_summary_
-        """
+        """handle current user
 
+        Returns:
+            TypeVar: User
+        """
+        request = Flask(__name__)
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request
+
+        Args:
+            request (request_object, optional): request. Defaults to None.
+        """
+        if request is None:
+            return None
+
+        session_name = getenv('SESSION_NAME')
+
+        if session_name is None:
+            return None
+
+        return request.cookies.get(session_name)
--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -5,27 +5,6 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-# unauthorized access
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """_summary_
-
-    Returns:
-        str: _description_
-    """
-    abort(401, description='Unauthorized')
-
-# forbidden
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """_summary_
-
-    Returns:
-        str: _description_
-    """
-    abort(403, description='Forbidden')
-
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -45,3 +24,26 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def test_unauthorized() -> str:
+    """raise error for unauthorized request
+
+    Returns:
+        str: 401 status code
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def handle_forbidden() -> str:
+    """handles forbidden requests
+
+    Args:
+        error (obj): error obj
+
+    Returns:
+        str: status code -> 403
+    """
+    abort(403)
